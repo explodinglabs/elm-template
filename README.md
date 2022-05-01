6c7459c081f2aa901e63b2ec25e5d3a9fdf03847
Beau Barker <beau@explodinglabs.com>
2022-05-01 10:06:43 +1000

    Initial import

---
+++ README.md
@@ -0,0 +1,27 @@
+<img
+    alt="My App"
+    style="margin: 0 auto;"
+    src="https://github.com/explodinglabs/my-app/blob/main/logo.png?raw=true"
+/>
+
+We use a custom nginx container to serve the static files built by Elm.
+
+Start the container (which starts nginx, due to the base image being nginx).
+For development we use a different nginx.conf which *doesn't* use SSL
+certificates or redirect port 80 to 443.
+```sh
+docker run --rm --name my-app --network explodinglabs --publish 80:80 -v ${PWD}/nginx-dev.conf:/etc/nginx/nginx.conf -v ${PWD}/static:/usr/share/nginx/html ghcr.io/explodinglabs/my-app
+```
+
+Visit [http://localhost/](http://localhost/).
+
+For production, and mount the LetsEncrypt keys into the container, and expose
+port 443:
+```sh
+docker run -d --name standup-web --network explodinglabs --publish 80:80 --publish 443:443 -v /etc/letsencrypt/live/standupmeeting.app/fullchain.pem:/certs/fullchain.pem -v /etc/letsencrypt/live/standupmeeting.app/privkey.pem:/certs/privkey.pem ghcr.io/explodinglabs/standup-web
+```
+
+To build the image:
+```sh
+docker build -t ghcr.io/explodinglabs/my-app .
+```
---
+++ src/Main.elm
@@ -0,0 +1,40 @@
+port module Main exposing (main)
+
+import Browser
+
+
+init : () -> Url -> Browser.Navigation.Key -> ( Model, Cmd Msg )
+init _ url _ =
+    let
+        route =
+            Url.Parser.parse urlParser url
+    in
+    case route of
+        Nothing ->
+            ( { zone = Time.utc, time = Time.millisToPosix 0, status = Failure ("Could not get route " ++ url.path) }, Cmd.none )
+
+        Just teamId ->
+            ( { zone = Time.utc, time = Time.millisToPosix 0, status = Loading }
+            , Cmd.batch
+                [ Task.perform GotZone Time.here
+                , Task.perform GotViewport Browser.Dom.getViewport
+                , Http.request
+                    { method = "GET"
+                    , headers = [ Http.header "Accept" "application/vnd.pgrst.object+json" ]
+                    , url = "/rest/teams?id=eq." ++ teamId ++ "&select=*,members(id,name,headline,photo,user_id)"
+                    , expect = Http.expectString GotTeam
+                    , body = Http.emptyBody
+                    , timeout = Nothing
+                    , tracker = Nothing
+                    }
+                ]
+            )
+
+
+main =
+    Browser.document
+        { init = init
+        , update = update
+        , subscriptions = subscriptions
+        , view = view
+        }
