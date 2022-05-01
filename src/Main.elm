db068d447b9c426355ae9929d9043ef62c3f5833
Beau Barker <beau@explodinglabs.com>
2022-05-01 11:21:26 +1000

    Adjustments

--- src/Main.elm
+++ src/Main.elm
@@ -1,35 +1,7 @@
-port module Main exposing (main)
+module Main exposing (main)
 
-import Browser
-
-
-init : () -> ( Model, Cmd Msg )
-init _ =
-    Nothing ->
-        ( { zone = Time.utc, time = Time.millisToPosix 0, status = Failure ("Could not get route " ++ url.path) }, Cmd.none )
-
-    Just teamId ->
-        ( { zone = Time.utc, time = Time.millisToPosix 0, status = Loading }
-        , Cmd.batch
-            [ Task.perform GotZone Time.here
-            , Task.perform GotViewport Browser.Dom.getViewport
-            , Http.request
-                { method = "GET"
-                , headers = [ Http.header "Accept" "application/vnd.pgrst.object+json" ]
-                , url = "/rest/teams?id=eq." ++ teamId ++ "&select=*,members(id,name,headline,photo,user_id)"
-                , expect = Http.expectString GotTeam
-                , body = Http.emptyBody
-                , timeout = Nothing
-                , tracker = Nothing
-                }
-            ]
-        )
+import Html exposing (text)
 
 
 main =
-    Browser.document
-        { init = init
-        , update = update
-        , subscriptions = subscriptions
-        , view = view
-        }
+    text "Hello, World!"
